8fc3a0e505b261e8e1c076892a9b5a2dc8052ac2
Alexander <alex.skhrv@gmail.com>
2014-11-29 12:15:53 +0300

    Stylized ActionBar

--- app/src/main/java/com/alexsu/weather/android/activity/MainActivity.java
+++ app/src/main/java/com/alexsu/weather/android/activity/MainActivity.java
@@ -1,12 +1,10 @@
 package com.alexsu.weather.android.activity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import com.alexsu.weather.android.R;
 import com.alexsu.weather.android.fragment.AbsLocationFragment;
